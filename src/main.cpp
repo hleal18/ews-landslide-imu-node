f93824cc30370eec3294db7ed0e45c31f0ad1df8
Humberto Leal <humberto.adolfo@hotmail.com>
2019-12-07 16:28:15 -0500

    Deletion of unnecesary lorawan event handlers

--- src/main.cpp
+++ src/main.cpp
@@ -41,36 +41,6 @@ void onEvent(ev_t ev)
 {
     switch (ev)
     {
-    case EV_SCAN_TIMEOUT:
-        Serial.println(F("EV_SCAN_TIMEOUT"));
-        break;
-    case EV_BEACON_FOUND:
-        Serial.println(F("EV_BEACON_FOUND"));
-        break;
-    case EV_BEACON_MISSED:
-        Serial.println(F("EV_BEACON_MISSED"));
-        break;
-    case EV_BEACON_TRACKED:
-        Serial.println(F("EV_BEACON_TRACKED"));
-        break;
-    case EV_JOINING:
-        Serial.println(F("EV_JOINING"));
-        break;
-    case EV_JOINED:
-        Serial.println(F("EV_JOINED"));
-        // Disable link check validation (automatically enabled
-        // during join, but not supported by TTN at this time).
-        LMIC_setLinkCheckMode(0);
-        break;
-    case EV_RFU1:
-        Serial.println(F("EV_RFU1"));
-        break;
-    case EV_JOIN_FAILED:
-        Serial.println(F("EV_JOIN_FAILED"));
-        break;
-    case EV_REJOIN_FAILED:
-        Serial.println(F("EV_REJOIN_FAILED"));
-        break;
     case EV_TXCOMPLETE:
         Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
         digitalWrite(BUILTIN_LED, LOW);
@@ -91,26 +61,6 @@ void onEvent(ev_t ev)
         delay(TX_INTERVAL * 1000);
         do_send(&sendjob);
         break;
-    case EV_LOST_TSYNC:
-        Serial.println(F("EV_LOST_TSYNC"));
-        break;
-    case EV_RESET:
-        Serial.println(F("EV_RESET"));
-        break;
-    case EV_RXCOMPLETE:
-        // data received in ping slot
-        Serial.println(F("EV_RXCOMPLETE"));
-        break;
-    case EV_LINK_DEAD:
-        Serial.println(F("EV_LINK_DEAD"));
-        break;
-    case EV_LINK_ALIVE:
-        Serial.println(F("EV_LINK_ALIVE"));
-        break;
-    default:
-        Serial.println(F("Unknown event"));
-        break;
-    }
 }
 
 void do_send(osjob_t *j)
